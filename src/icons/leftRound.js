67a0b52c20d77b3fdd958d36a0ced0385ee95f33
Kevin <kevin088.rejoice@gmail.com>
2026-01-26 16:05:51 +0530

    styles:svg path issue fixed

--- src/icons/leftRound.js
+++ src/icons/leftRound.js
@@ -7,12 +7,12 @@ export default function LeftRound() {
             <circle cx="141.5" cy="29.5" r="29" stroke="url(#paint1_linear_4130_568)" />
             <defs>
                 <linearGradient id="paint0_linear_4130_568" x1="112" y1="28.5" x2="-4.37114e-08" y2="28.5" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#252525" />
-                    <stop offset="1" stop-color="#F1F1F1" stop-opacity="0" />
+                    <stop stopColor="#252525" />
+                    <stop offset="1" stopColor="#F1F1F1" stopOpacity="0" />
                 </linearGradient>
                 <linearGradient id="paint1_linear_4130_568" x1="171" y1="29.5" x2="112" y2="29.5" gradientUnits="userSpaceOnUse">
-                    <stop offset="0.393941" stop-color="#F1F1F1" stop-opacity="0" />
-                    <stop offset="1" stop-color="#252525" />
+                    <stop offset="0.393941" stopColor="#F1F1F1" stopOpacity="0" />
+                    <stop offset="1" stopColor="#252525" />
                 </linearGradient>
             </defs>
         </svg>
--- src/icons/rightRound.js
+++ src/icons/rightRound.js
@@ -7,12 +7,12 @@ export default function RightRound() {
             <circle cx="29.5" cy="29.5" r="29" transform="matrix(-1 0 0 1 59 0)" stroke="url(#paint1_linear_4130_571)" />
             <defs>
                 <linearGradient id="paint0_linear_4130_571" x1="0" y1="0.5" x2="112" y2="0.5" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#252525" />
-                    <stop offset="1" stop-color="#F1F1F1" stop-opacity="0" />
+                    <stop stopColor="#252525" />
+                    <stop offset="1" stopColor="#F1F1F1" stopOpacity="0" />
                 </linearGradient>
                 <linearGradient id="paint1_linear_4130_571" x1="59" y1="29.5" x2="0" y2="29.5" gradientUnits="userSpaceOnUse">
-                    <stop offset="0.393941" stop-color="#F1F1F1" stop-opacity="0" />
-                    <stop offset="1" stop-color="#252525" />
+                    <stop offset="0.393941" stopColor="#F1F1F1" stopOpacity="0" />
+                    <stop offset="1" stopColor="#252525" />
                 </linearGradient>
             </defs>
         </svg>
