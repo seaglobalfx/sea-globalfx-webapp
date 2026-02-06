7301b58ed5beb2bc6f74ca56a227a13a46042550
Kevin <kevin088.rejoice@gmail.com>
2026-02-06 16:55:53 +0530

    fix: correct formatting and indentation in layout.js for improved readability

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
+               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-E27QCE3JE1');
@@ -37,10 +37,10 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-MMKX98B2');
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MMKX98B2');
             `,
           }}
         />
@@ -61,9 +61,9 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-        <noscript>
-          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=4170972363233509&ev=PageView&noscript=1" />
-        </noscript>
+        <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=4170972363233509&ev=PageView&noscript=1"
+        /></noscript>
         {/* Organization Schema */}
         <script
           type="application/ld+json"
