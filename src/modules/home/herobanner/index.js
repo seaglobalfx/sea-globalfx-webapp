913c674a70a71e5e09037faaf8473c185afe7912
Kevin <kevin088.rejoice@gmail.com>
2026-05-29 09:43:17 +0530

    styles:mobile view counter added

--- src/modules/home/herobanner/index.js
+++ src/modules/home/herobanner/index.js
@@ -6,6 +6,7 @@ import styles from './herobanner.module.scss';
 import MobileAnimation from './mobileAnimation';
 
 import { useLanguage } from '@/context/LanguageContext';
+import CountUp from 'react-countup';
 
 const HeroImage = '/assets/images/hero-coin.gif';
 const UserIcon = '/assets/icons/user.svg';
@@ -44,68 +45,138 @@ export default function Herobanner() {
     const { t } = useLanguage();
     return (
         <div className={styles.spacingRemove}>
-        <div className='container-md'>
-            <div className={styles.herobanner}>
-                <div className={styles.leftAlignment}>
-                    <div className={styles.grid}>
+            <div className='container-md'>
+                <div className={styles.herobanner}>
+                    <div className={styles.leftAlignment}>
+                        <div className={styles.grid}>
 
-                        {/* LEFT CONTENT */}
-                        <motion.div
-                            className={styles.griditems}
-                            variants={container}
-                            initial="hidden"
-                            animate="visible"
-                        >
-                            <div>
-                                <motion.div className={styles.tagline} variants={fadeUp}>
-                                    <button>
-                                        {t('hero.tagline')}
-                                    </button>
-                                </motion.div>
+                            {/* LEFT CONTENT */}
+                            <motion.div
+                                className={styles.griditems}
+                                variants={container}
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                <div>
+                                    <motion.div className={styles.tagline} variants={fadeUp}>
+                                        <button>
+                                            {t('hero.tagline')}
+                                        </button>
+                                    </motion.div>
 
-                                <motion.h1 variants={fadeUp}>
-                                    {t('hero.titleStart')}<span>{t('hero.titleSpan')}</span>{t('hero.titleEnd')}
-                                </motion.h1>
+                                    <motion.h1 variants={fadeUp}>
+                                        {t('hero.titleStart')}<span>{t('hero.titleSpan')}</span>{t('hero.titleEnd')}
+                                    </motion.h1>
 
-                                <motion.p variants={fadeUp}>
-                                    {t('hero.desc')}
-                                </motion.p>
+                                    <motion.p variants={fadeUp}>
+                                        {t('hero.desc')}
+                                    </motion.p>
 
-                                <motion.div
-                                    className={styles.twoButtonalignment}
-                                    variants={container}
-                                >
-                                    <a href='https://client.seaglobalfx.com/?tab=register' target='_blank'>
-                                        <motion.button className={styles.orange} variants={fadeUp}>
-                                            <img src={UserIcon} alt='UserIcon' />
-                                            {t('hero.openAccount')}
-                                        </motion.button>
-                                    </a>
-                                    <a target='_blank' href='https://client.seaglobalfx.com/'>
-                                        <motion.button className={styles.black} variants={fadeUp}>
-                                            <img src={DemoIcon} alt='DemoIcon' />
-                                            {t('hero.tryDemo')}
-                                        </motion.button>
-                                    </a>
-                                </motion.div>
 
-                            </div>
-                        </motion.div>
 
-                        {/* RIGHT IMAGE */}
-                        <motion.div
-                            className={styles.griditems}
-                            variants={imageAnim}
-                            initial="hidden"
-                            animate="visible"
-                        >
-                            <MobileAnimation />
-                        </motion.div>
+                                    <motion.div
+                                        className={styles.twoButtonalignment}
+                                        variants={container}
+                                    >
+                                        <a href='https://client.seaglobalfx.com/?tab=register' target='_blank'>
+                                            <motion.button className={styles.orange} variants={fadeUp}>
+                                                <img src={UserIcon} alt='UserIcon' />
+                                                {t('hero.openAccount')}
+                                            </motion.button>
+                                        </a>
+                                        <a target='_blank' href='https://client.seaglobalfx.com/'>
+                                            <motion.button className={styles.black} variants={fadeUp}>
+                                                <img src={DemoIcon} alt='DemoIcon' />
+                                                {t('hero.tryDemo')}
+                                            </motion.button>
+                                        </a>
+                                    </motion.div>
+                                    <div className={styles.counterSection}>
+                                        <div>
 
+                                            <h5>
+                                                <CountUp
+                                                    start={0}
+                                                    end={4.8}
+                                                    decimals={1}
+                                                    duration={2}
+                                                    enableScrollSpy={true}
+                                                    scrollSpyOnce={true}
+                                                />
+                                                /5
+                                            </h5>
+                                            <h6>
+                                                {t('hero.rating')}
+                                            </h6>
+                                        </div>
+                                        <div>
+                                            <h5>
+
+                                                <CountUp
+                                                    start={0}
+                                                    end={120}
+                                                    duration={2}
+                                                    enableScrollSpy={true}
+                                                    scrollSpyOnce={true}
+                                                />
+                                                +
+
+                                            </h5>
+                                            <h6>
+                                                {t('hero.countries')}
+                                            </h6>
+                                        </div>
+                                        <div>
+
+                                            <h5>
+                                                <CountUp
+                                                    start={0}
+                                                    end={500}
+                                                    duration={2}
+                                                    enableScrollSpy={true}
+                                                    scrollSpyOnce={true}
+                                                />
+                                                M +
+                                            </h5>
+                                            <h6>
+                                                {t('hero.totalTrades')}
+                                            </h6>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                {`<`}
+                                                <CountUp
+                                                    start={0}
+                                                    end={15}
+                                                    duration={2}
+                                                    enableScrollSpy={true}
+                                                    scrollSpyOnce={true}
+                                                />
+                                                MS
+                                            </h5>
+                                            <h6>
+                                                {t('hero.execution')}
+                                            </h6>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </motion.div>
+
+                            {/* RIGHT IMAGE */}
+                            <motion.div
+                                className={styles.griditems}
+                                variants={imageAnim}
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                <MobileAnimation />
+                            </motion.div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 }
